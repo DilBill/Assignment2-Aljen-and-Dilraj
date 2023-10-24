bc7c53b63da067e56d7959573c123c1d4b831336
DilBil <dilrajbola25@gmail.com>
2023-10-24 17:50:41 -0400

    Info.py v1

---
+++ info.py
@@ -0,0 +1,11 @@
+'''The class User is used to retain the information of the user such as the name, email, and address'''
+class User:
+    def __init__(self, name, email, address):
+        self.userName = name
+        self.userEmail = email
+        self.userAddress = address
+        
+'''The class Pizza is used to create the order including the size, amount and total price before or after discounts'''
+class Order:
+
+    
