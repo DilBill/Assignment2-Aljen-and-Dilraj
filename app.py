dca1c8fbb75edc59150ace506a8b8ceb86f6fb18
DilBil <dilrajbola25@gmail.com>
2023-10-24 18:10:54 -0400

    creating the selection statements

--- app.py
+++ app.py
@@ -7,6 +7,35 @@ email = input('please provide a email so that we can send you updates on the ord
 # ask the use for an address for delivery
 address = input('Please provide an address for delivery: ')
 
+while True:
+    # ask the user what size pizza they want
+    size = int(input('What size pizza would you like to order\n 1 for SMALL / 2 for MEDIUM / 3 for LARGE / 4 for X-LARGE : '))
+    # conditonal statement checking if the user inputed 1
+    if size == 1:
+        # Changes the variable price depending on the selection
+        price = 10
+        print(f'One small pizza will cost ${price}')
+        break
+    # conditonal statement checking if the user inputed 2
+    elif size == 2:
+        price = 12
+        print(f'One medium pizza will cost ${price}')
+        break
+    # conditonal statement checking if the user inputed 3
+    elif size == 3:
+        price = 15
+        print(f'One large pizza will cost ${price}')
+        break
+    # conditonal statement checking if the user inputed 4
+    elif size == 4:
+        price = 18
+        print(f'One x-large pizza will cost ${price}')
+        break
+    # conditonal statement checking if the user input something other than 1, 2, 3, 4 and will allow them to try again
+    elif size != 1 or size != 2 or size != 3 or size != 4:
+        print('Invaild Size Try Again')
+
+
 
     
     
