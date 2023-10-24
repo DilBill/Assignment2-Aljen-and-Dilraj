39a7e3dc3e0f9bfcbf09307f03de31f9c098b97c
DilBil <dilrajbola25@gmail.com>
2023-10-24 18:10:26 -0400

    app.py v1

---
+++ app.py
@@ -0,0 +1,12 @@
+# Import the User and Pizza classes from the info.py file
+from info import User, Order
+# ask the user for their name
+name = input('Whats the name for the order? ')
+# ask the user to provide a email
+email = input('please provide a email so that we can send you updates on the order\n (exampleEmail@gmail.com): ')
+# ask the use for an address for delivery
+address = input('Please provide an address for delivery: ')
+
+
+    
+    
